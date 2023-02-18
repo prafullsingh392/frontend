e839dd786aaab376f8f6ddc6d20aa9d28462d306
B0265984 <b0265984@LTB0265984-MAC.local>
2023-02-19 00:16:52 +0530

    added routings

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { MaterialeModule } from 'src/material.module';
+import { SearchResultComponent } from './search-result/search-result.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    SearchResultComponent
   ],
   imports: [
     MaterialeModule,
